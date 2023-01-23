71547b593a7af661373bb6b58fade5c5025775d5
bmaggiano <brandon.maggiano@gmail.com>
2023-01-23 11:59:25 -0700

    initial commit

---
+++ client/src/components/ThoughtForm/index.js
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
+
+const ThoughtForm = () => {
+  const [thoughtText, setThoughtText] = useState({
+    thought: '',
+    hours: '',
+    patients: '',
+  });
+
+
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+      try {
+        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+        cache.writeQuery({
+          query: QUERY_THOUGHTS,
+          data: { thoughts: [addThought, ...thoughts] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      // update me object's cache
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+      });
+    },
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addThought({
+        variables: {
+          thoughtText,
+          thoughtAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setThoughtText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (e) => {
+    // const { name, value } = event.target;
+    setThoughtText({
+      ...thoughtText,
+      [e.target.name]: e.target.value,
+  });
+  };
+
+  return (
+    <div>
+      <h3>Worker Form</h3>
+
+      {Auth.loggedIn() ? (
+        <>
+
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <input
+              name="hours"
+              placeholder='hours worked'
+              value={thoughtText.hours}
+              className="form-input w-100"
+              onChange={handleChange}
+              style={{ lineHeight: '1.5', resize: 'vertical' }}
+              ></input>
+              <input
+              name="patients"
+              placeholder='patients worked'
+              value={thoughtText.patients}
+              className="form-input w-100"
+              onChange={handleChange}
+              style={{ lineHeight: '1.5', resize: 'vertical' }}
+              ></input>
+              <textarea
+                name="thought"
+                placeholder="Lexiscan Remaining"
+                value={thoughtText.thought}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+                Add Workday
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to share your thoughts. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default ThoughtForm;
